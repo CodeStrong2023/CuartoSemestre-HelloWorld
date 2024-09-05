24fbde21de35b5c225f27978d6f143fc7fda1399
Flor <popiflorencia@yahoo.com.ar>
2024-09-04 22:31:17 -0300

    Clase 03 E-commerce Básico Parte 03
    Clase 03 de JavaScript: E-commerce Básico Parte 03

---
+++ JavaScript/e-comerce/client/js/products.js
@@ -0,0 +1,24 @@
+const productos = [
+    {
+        id:1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/bananas.jpeg" 
+    },
+    {
+        id:2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/Leche.jpg" 
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quanty: 1,
+        img: "/client/media/pollo.jpeg" 
+
+    }
+]
