24b7a7d3bb9a1906c94547bd82f71bd183d1572e
leoeugenio16 <leoeugenio16@gmail.com>
2024-09-06 12:48:37 -0300

    Clase 04 Aplicación de Sistema Estudiantes con Spring Boot -> Parte 3

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -4,7 +4,7 @@ import utn.estudiantes.modelo.Estudiante;
 import java.util.List;
 
 public interface IEstudianteServicio {
-    public List<EstudianteServicio> listarEstudiantes();
+    public List<Estudiante> listarEstudiantes();
     public Estudiante buscarEstudiantePorId(Integer idEstudiante);
     public void guardarEstudiante(Estudiante estudiante);
     public void eliminarEstudiante(Estudiante estudiante);
