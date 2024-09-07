6ea548cef0bc1290c72bb00d1fb9f7f28fac3154
Flor <popiflorencia@yahoo.com.ar>
2024-09-07 16:38:51 -0300

    Java Clase 4 Parte 3
    Clase 04 Aplicación de Sistema Estudiantes con Spring Boot -> Parte 3

---
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,27 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+//biolerplate - codigo Repetitivo con lombok
+@Data //genera setter y getters
+@NoArgsConstructor //genera constructor vacio
+@AllArgsConstructor //genera el constgructor con todos los argumentos
+@ToString //genera el toString
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+}
