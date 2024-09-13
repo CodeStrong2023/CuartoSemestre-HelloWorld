2110ab1916cc0ffcb87cf6ec0f2f21debc39b808
leoeugenio16 <leoeugenio16@gmail.com>
2024-09-13 07:08:50 -0300

    Clase 03 Comenzamos con pruebas en ejecución

---
+++ Node/01-Hola-Mundo/app3.js
@@ -0,0 +1,12 @@
+console.log('Inicio del programa')
+setTimeout(()=>{
+    console.log("Primer Timeout")
+},3000)
+setTimeout(()=>{
+    console.log("Seundo Timeout")
+},3000)
+setTimeout(()=>{
+    console.log("Tercero Timeout")
+},3000)
+
+console.log('Fin del programa')
