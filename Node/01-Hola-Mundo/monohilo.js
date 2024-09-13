118e6566630671cd583c188bc775dc073b80c311
leoeugenio16 <leoeugenio16@gmail.com>
2024-09-13 07:16:57 -0300

    Clase 05 Ejecutamos viendo la asincronía de Node

---
+++ Node/01-Hola-Mundo/monohilo.js
@@ -0,0 +1,15 @@
+console.log('Hola a toda la cohorte 2023')
+
+var i =  1;
+setInterval(() => {
+    console.log('Sigo activo')
+    console.log(i)
+    i++;
+
+    /* if(i === 5){
+        console.log('Forzamos un error')
+        var a = 3 + z
+    } */
+}, 1000);
+
+console.log('Segunda instruccion')
