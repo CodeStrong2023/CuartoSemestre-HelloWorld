4ff40ef715418efe27516c3999a328d4456bb151
leoeugenio16 <leoeugenio16@gmail.com>
2024-09-18 12:15:10 -0300

    backend setup y express server

---
+++ Node/PERN-Srack/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"))
+app.use(express)
+app.use(express.urlencoded({extended: false}))
+app.get("/", (req, res) => res.json({ message: "Bienvenido a mi proyecto" }));
+
+app.get('/test', (req, res) => {
+    throw new Error("Error generado por el usuario")
+
+    res.send('test');
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        status: "error",
+        message: err.message
+    }
+    )
+}
+);
+
+export default app;
--- Node/PERN-Srack/src/index.js
+++ Node/PERN-Srack/src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello  World')
+
+import app from "./app.js";
+
+app.listen(3000);
+
+console.log("server lanzando el puerto " + 3000)
