19bab615e56862f23398d922290ee0025611d30b
Flor <popiflorencia@yahoo.com.ar>
2024-09-30 23:43:13 -0300

    Clase 4 - E-commerce Básico
    Clase 4 de JavaScript- E-commerce Básico parte 4

--- JavaScript/e-comerce/client/js/cart.js
+++ JavaScript/e-comerce/client/js/cart.js
@@ -1,6 +1,7 @@
 const modalContainer = document.getElementById("modal-container");
 const modalOverlay = document.getElementById("modal-overlay");
 const cartBtn = document.getElementById("cart-btn");
+const cartCounter = document.getElementById("cart-counter");
 
 
 const displayCart = () => {
@@ -15,10 +16,10 @@ const displayCart = () => {
     modalClose.className="modal-close";
 
     modalHeader.append(modalClose);
-    modalClose.addEventListener("click", () =>{
+    modalClose.addEventListener("click", () => {
         modalContainer.style.display= "none";
         modalOverlay.style.display = "none";
-    })
+    });
 
     const modalTitle = document.createElement("div");
     modalTitle.innerText="Carrito";
@@ -28,6 +29,7 @@ const displayCart = () => {
     modalContainer.append(modalHeader);
 
     // Modal Body
+if (cart.length > 0) {
 cart.forEach((product) => {
     const modalBody = document.createElement("div");
     modalBody.className = "modal-body";
@@ -60,27 +62,92 @@ cart.forEach((product) => {
     increse.addEventListener("click", () => {
         product.quanty++;
         displayCart();
+        displayCartCounter();
     });
 
     //delete
     const deleteProduct = modalBody.querySelector(".delete-product");
 
     deleteProduct.addEventListener("click", () => {
-        deleteCartProduct(product.id)
-    })
+        deleteCartProduct(product.id);
+    });
 });
 
 //modal fotter
 const total = cart.reduce((acc, el) => acc + el.price * el.quanty, 0);
 
 const modalFooter = document.createElement("div");
-modalFooter.className = "modal-footer"
+modalFooter.className = "modal-footer";
 modalFooter.innerHTML = `
 <div class="total-price">${total}</div>
-
+<button class="btn-primart" id="checkout-btn"> go to checkout</button>
+<div id="button.checkout"></div>
 `;
 modalContainer.append(modalFooter);
-};
+// mp;
+const mercadopago = new MercadoPagoConfig("TLdig0sWoJ",{
+    locale: "es-AR", //The most common are: "pt-BR", "es-AR" and "en-US"
+});
+
+const checkoutButton = modalFooter.querySelector("#checkout.btn");
+checkoutButton.addEventListener("click", function (){
+    checkoutButton.remove();
+
+    const orderData = {
+        quantity: 1,
+        description: "compra de ecommerce",
+        price: total,
+    };
+
+    fetch("http://localhost:8080/create_preference", {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (preference) {
+            createCheckoutButton(preference.id);
+        })
+        .catch(function () {
+            alert("Unexpected error");
+        });
+});
+
+function createCheckoutButton(preferenceId){
+    const bricksBuilder = mercadopago.bricks();
+
+    const renderComponent = async (bricksBuilder) => {
+        //if (window.checkoutButton) checkoutButton.unmount();
+
+        await bricksBuilder.create{
+            "wallet",
+            "button.checkout",
+            {
+                initialization: {
+                    preferenceId: preferenceId,
+                },
+                callbacks: {
+                    onError: (error) => console.error(error),
+                    onReady: () => {},
+                },
+            }
+        };
+    };
+    window.checkoutButton = renderComponent(bricksBuilder);
+}
+
+}else {
+    const modalText = document.createElement("h2")
+    modalText.className = "modal-body";
+    modalText.innerText = "Tu carrito esta vacío";
+    modalContainer.append(modalText);
+}
+
+}
 
 cartBtn.addEventListener("click",displayCart);
 
@@ -88,4 +155,14 @@ const deleteCartProduct = (id) => {
     const foundId = cart.findIndex((element)=> element.id === id)
     cart.slice(foundId, 1);
     displayCart();
-}
+};
+
+const displayCartCounter =() => {
+    const cartLength = cart.reduce((acc, el) => acc + el.quanty, 0);
+    if(cartLength > 0){
+        cartCounter.style.display = "block";
+        cartCounter.innerText = cartLength;
+    } else{
+        cartCounter.style.display = "none";
+    }
+};
