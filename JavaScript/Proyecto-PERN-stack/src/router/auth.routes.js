3333f7daa3fd2a31c23a1b5879f20d17a27b9c95
Flor <popiflorencia@yahoo.com.ar>
2024-11-21 23:53:40 -0300

    Clase 08 JavaScript
    Clase 08 Proyecto con stack pern por Daniel Guerrero -> Parte 4

--- JavaScript/Proyecto-PERN-stack/src/router/auth.routes.js
+++ JavaScript/Proyecto-PERN-stack/src/router/auth.routes.js
@@ -1,5 +1,6 @@
 import Router from "express-promise-router";
 import { signin, signup, signout, profile } from "../controllers/auth.controller.js";
+import { isAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -9,7 +10,7 @@ router.post("/signup", signup);
 
 router.post("/signout", signout);
 
-router.get("/profile", profile);
+router.get("/profile", isAuth, profile);
 
 
 
